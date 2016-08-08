8515b20db9ed6b6bade3e8106256b6893345af1d
Peter Halliday <pghalliday@gmail.com>
2016-08-08 18:19:16 +0200

    integrating new plugin and stb tools

--- karma.conf.js
+++ karma.conf.js
@@ -8,8 +8,7 @@ const browsers = [
 ];
 
 let upstreamProxy = undefined;
-let customLaunchers = undefined;
-let stbReporter = undefined;
+let stb = undefined;
 
 // Are we runnning tests on the STB
 if (process.env['STB_TEST']) {
@@ -27,20 +26,14 @@ if (process.env['STB_TEST']) {
 
   // add the stb reporter to collect timestamped test logs
   reporters.push('stb');
-  stbReporter = {
-    reportsDir: './reports'
-  };
 
   // add the stb launcher to run tests on the STB too
-  browsers.push('MyStb');
-  customLaunchers = {
-    MyStb: {
-      base: 'stb',
-      stb: stbConfig.ip, // STB IP address
-      server: stbConfig.localIp, // Karma server IP address as seen by STB
-      // Additional TR-69 parameters to set (`UIServerURL` is constructed from config)
-      tr69: {}
-    }
+  browsers.push('stb');
+
+  // configure the STB plugin
+  stb = {
+    reports: './reports',
+    profile: 'latest'
   };
 }
 
@@ -50,9 +43,8 @@ module.exports = function (config) {
   config.set({
     upstreamProxy: upstreamProxy,
     browsers: browsers,
-    customLaunchers: customLaunchers,
     reporters: reporters,
-    stbReporter: stbReporter,
+    stb: stb,
 
     frameworks: ['mocha'],
 
@@ -73,7 +65,7 @@ module.exports = function (config) {
 
     logLevel: config.LOG_INFO,
 
-    singleRun: true,
+    singleRun: false,
 
     // Avoid DISCONNECTED messages
     // See https://github.com/karma-runner/karma/issues/598
